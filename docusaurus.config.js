436046487f0ca142915449df5e8d6e84e17bc71b
Derek Kershner <dkershner@dkershner.com>
2021-06-05 11:10:08 -0700

    Initial Commit, TypeScript

---
+++ docusaurus.config.js
@@ -0,0 +1,102 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+    title: "Resonance Documentation",
+    tagline: "Articles and References for the Resonance Platform",
+    url: "https://docs.buildresonance.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "Resonance", // Usually your GitHub org/user name.
+    projectName: "resonance-webapp-docs", // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: "Resonance Documentation",
+            logo: {
+                alt: "Resonance",
+                src: "img/logo.svg",
+            },
+            items: [
+                {
+                    type: "doc",
+                    docId: "intro",
+                    position: "left",
+                    label: "Tutorial",
+                },
+                { to: "/blog", label: "Blog", position: "left" },
+                {
+                    href: "https://github.com/facebook/docusaurus",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
+        },
+        footer: {
+            style: "dark",
+            links: [
+                {
+                    title: "Docs",
+                    items: [
+                        {
+                            label: "Tutorial",
+                            to: "/docs/intro",
+                        },
+                    ],
+                },
+                {
+                    title: "Community",
+                    items: [
+                        {
+                            label: "Stack Overflow",
+                            href: "https://stackoverflow.com/questions/tagged/docusaurus",
+                        },
+                        {
+                            label: "Discord",
+                            href: "https://discordapp.com/invite/docusaurus",
+                        },
+                        {
+                            label: "Twitter",
+                            href: "https://twitter.com/docusaurus",
+                        },
+                    ],
+                },
+                {
+                    title: "More",
+                    items: [
+                        {
+                            label: "Blog",
+                            to: "/blog",
+                        },
+                        {
+                            label: "GitHub",
+                            href: "https://github.com/facebook/docusaurus",
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Resonance.`,
+        },
+    },
+    presets: [
+        [
+            "@docusaurus/preset-classic",
+            {
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    editUrl:
+                        "https://github.com/facebook/docusaurus/edit/master/website/",
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl:
+                        "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            },
+        ],
+    ],
+};
